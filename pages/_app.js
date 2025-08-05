103ad1a9f6094d131bb27372b705651bd8ac7163
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2025-08-05 21:27:53 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 // pages/_app.js
+import { useEffect } from 'react';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-XXXXXXXXXX'); // 🔁 Replace with your GA4 Measurement ID
+  }, []);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" // 🔁 Replace with your ID
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
